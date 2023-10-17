2e58bb73fe48200a48d51ed5d7c9d32486e5bcb9
pragnasree07 <pragnapallam0210@gmail.com>
2023-10-17 13:08:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Project from "./components/Project";
 import Skills from "./components/skills"
 // import Cards from "./components/cards";
 import './App.css';
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
